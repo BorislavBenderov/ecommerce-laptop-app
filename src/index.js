8c5a158a6feb2cba165e042d439a8ccb0590ff47
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-17 20:59:20 +0200

    Use hashrouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import 'font-awesome/css/font-awesome.css';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
@@ -9,8 +9,8 @@ import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </Provider>
 );
