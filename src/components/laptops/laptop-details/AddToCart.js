441d17957497c208d7924b16a2edaa3dbb033e8d
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-17 18:30:24 +0200

    Add is laptop including in the cart logic

--- src/components/laptops/laptop-details/AddToCart.js
+++ src/components/laptops/laptop-details/AddToCart.js
@@ -1,4 +1,4 @@
-import { arrayUnion, doc, updateDoc } from "firebase/firestore";
+import { addDoc, collection, doc, updateDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 import { database } from "../../../firebaseConfig";
 import toast, { Toaster } from 'react-hot-toast';
@@ -6,24 +6,39 @@ import { useSelector } from "react-redux";
 
 export const AddToCart = ({ currentLaptop }) => {
     const loggedUser = useSelector((store) => store.user.user);
+    const { cart } = useSelector((store) => store.cart);
     const navigate = useNavigate();
+    const isIncluding = cart.find(user => user.uid === loggedUser?.uid && user.title === currentLaptop.title);
 
     const addedToCart = () => {
         if (loggedUser) {
-            updateDoc(doc(database, 'users', loggedUser.uid), {
-                cart: arrayUnion({
+            if (isIncluding) {
+                updateDoc(doc(database, 'users', isIncluding.id), {
+                    price: isIncluding.price + Number(currentLaptop.price),
+                    quantity: isIncluding.quantity + Number(currentLaptop.quantity),
+                })
+                    .then(() => {
+                        console.log('updated')
+                    })
+                    .catch((err) => {
+                        alert(err.message);
+                    })
+            } else {
+                addDoc(collection(database, 'users'), {
                     title: currentLaptop.title,
-                    price: currentLaptop.price,
-                    id: currentLaptop.id,
+                    price: Number(currentLaptop.price),
+                    quantity: Number(currentLaptop.quantity),
+                    laptopId: currentLaptop.id,
+                    uid: loggedUser.uid,
                     image: currentLaptop.image
                 })
-            })
-                .then(() => {
-                    toast.success(`${currentLaptop.title} added to cart!`);
-                })
-                .catch((err) => {
-                    alert(err.message);
-                })
+                    .then(() => {
+                        toast.success(`${currentLaptop.title} added to cart!`);
+                    })
+                    .catch((err) => {
+                        alert(err.message);
+                    })
+            }
         } else {
             navigate('/login');
         }
