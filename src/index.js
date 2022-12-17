5c1dd16b11f98bdbf8a2e7b0941adb20a7315d4b
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-17 10:31:10 +0200

    Provide store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import 'font-awesome/css/font-awesome.css';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <HashRouter>
-        <App />
-    </HashRouter>
-
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 );
