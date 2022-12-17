539a416632b40294921b0b6aba8af549f84ce68c
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-17 18:27:45 +0200

    Update laptopSlice.js
    Add get all laptops action

--- src/feautures/laptops/laptopSlice.js
+++ src/feautures/laptops/laptopSlice.js
@@ -1,24 +1,10 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { database } from '../../firebaseConfig';
-import { getDocs, collection } from "firebase/firestore";
+import { createSlice } from "@reduxjs/toolkit";
+
 
 const initialState = {
     laptops: [],
     currentLaptop: [],
-    isLoading: false
 };
-
-export const getLaptops = createAsyncThunk('data/getData', async () => {
-    try {
-        const data = await getDocs(collection(database, 'laptops'));
-        const result = data.docs.map((item) => {
-            return { ...item.data(), id: item.id }
-        });
-        return result;
-    } catch (error) {
-        alert(error.message);
-    }
-});
 
 export const laptopSlice = createSlice({
     name: 'laptops',
@@ -26,23 +12,13 @@ export const laptopSlice = createSlice({
     reducers: {
         getCurrentLaptop: (state, action) => {
             state.currentLaptop = action.payload;
+        },
+        getAllLaptops: (state, action) => {
+            state.laptops = action.payload
         }
-    },
-    extraReducers: (builder) => {
-        builder.addCase(getLaptops.pending, (state) => {
-            state.isLoading = false;
-        })
-            .addCase(getLaptops.fulfilled, (state, action) => {
-                state.isLoading = true;
-                state.laptops = action.payload;
-            })
-            .addCase(getLaptops.rejected, (state, action) => {
-                state.isLoading = false;
-                alert(action.payload);
-            })
     }
 });
 
-export const { getCurrentLaptop } = laptopSlice.actions;
+export const { getAllLaptops, getCurrentLaptop } = laptopSlice.actions;
 
 export default laptopSlice.reducer;
