b8c4a6dd54726894323a7d9ce4958396032265d0
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-17 18:29:30 +0200

    Render laptops in the cart

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,23 +1,26 @@
-import { useContext } from 'react';
-import { UserContext } from '../../contexts/UserContext';
 import { CartItem } from './CartItem';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export const Cart = () => {
-    const { currentUser, totalPrice } = useContext(UserContext);
+    const { cart } = useSelector((store) => store.cart);
+    const loggedUser = useSelector((store) => store.user.user);
+    const userCart = cart.filter(user => user.uid === loggedUser.uid);
+    const totalPrice = userCart?.reduce((acc, curVal) => acc + Number(curVal.price),
+        0);
 
     return (
         <div className="cart-container">
             <h2>Your Cart</h2>
             <div className="product-container">
-                {currentUser?.cart?.length > 0
-                    ? currentUser.cart.map(laptop => <CartItem key={laptop.id} laptop={laptop} />)
+                {cart?.length > 0
+                    ? cart.map(laptop => <CartItem key={laptop.id} laptop={laptop} />)
                     : <>
                         <p>No laptops in the cart!</p>
                         <Link to='/' className="btn">Shop Laptops</Link>
                     </>}
             </div>
-            {currentUser?.cart?.length > 0
+            {cart?.length > 0
                 ? <div className="cart-bottom">
                     <div className="total">
                         <h3>Subtotal:</h3>
